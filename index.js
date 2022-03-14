34b967bab4d0685d002e48adc99026a52890c861
John mwadime <johnniekips@gmail.com>
2022-03-14 12:42:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ const server = app.listen(5000);
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
+const apiai = require('apiai')(APIAI_TOKEN);
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+
+    // Get a reply from API.AI
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      socket.emit('bot reply', aiText); // Send the result back to the browser!
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
